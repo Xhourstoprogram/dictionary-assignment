80bf20a711fce09669e303795fed832192e769a7
Reynard Timotius <91584193+Xhourstoprogram@users.noreply.github.com>
2021-11-06 16:21:33 +0700

    Add files via upload

---
+++ car models-Reynard.py
@@ -0,0 +1,55 @@
+Anthony = {
+    "1": "Anthony likes these cars:",
+    "Car 1": "- Toyota MR2",
+    "Car 2": "- Mazda MX5",
+    "Car 3": "- Honda Civic"
+}
+
+Jimmy = {
+    "2": "Jimmy likes these cars:",
+    "Car 1": "- Nissan Skyline GTR",
+    "Car 2": "- Toyota Supra",
+    "Car 3": "- Mitsubishi Lancer Evolition 8"
+}
+
+Matthew = {
+    "3": "Matthew likes these cars:",
+    "Car 1": "- Mazda RX7",
+    "Car 2": "- Mercedes AMG",
+    "Car 3": "- Honda NSX"
+}
+
+Garry = {
+    "4": "Garry likes these cars:",
+    "Car 1": "- Mercedes Benz",
+    "Car 2": "- Aston Martin",
+    "Car 3": "- Mazda RX8"
+}
+
+Arthur = {
+    "5": "Arthur likes these cars:",
+    "Car 1": "- BMW",
+    "Car 2": "- Mitsubishi Lancer Evolution 9",
+    "Car 3": "- Porsche Carrera GT"
+}
+
+print(Anthony["1"])
+print(Anthony["Car 1"])
+print(Anthony["Car 2"])
+print(Anthony["Car 3"])
+print(Jimmy["2"])
+print(Jimmy["Car 1"])
+print(Jimmy["Car 2"])
+print(Jimmy["Car 3"])
+print(Matthew["3"])
+print(Matthew["Car 1"])
+print(Matthew["Car 2"])
+print(Matthew["Car 3"])
+print(Garry["4"])
+print(Garry["Car 1"])
+print(Garry["Car 2"])
+print(Garry["Car 3"])
+print(Arthur["5"])
+print(Arthur["Car 1"])
+print(Arthur["Car 2"])
+print(Arthur["Car 3"])
---
+++ programming language-Reynard.py
@@ -0,0 +1,35 @@
+Java = {
+    "1": "Javascript",
+    "def": 
+    "A high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible"
+    }
+Go = {
+    "2": "Go",
+    "def": "A statically typed, compiled programming language designed at Google"
+    }
+HTML = {
+    "3": "HTML",
+    "def": "The most basic building block of the Web. It defines the meaning and structure of web content."
+    }
+Python = {
+    "4": "Python",
+    "def": "An interpreted high-level general-purpose programming language."
+    } 
+CSS = {
+    "5": "CSS",
+    "def": "A style sheet language used for describing the presentation of a document written in a markup language such as HTML."
+    }
+print(HTML["3"])
+print(HTML["def"])
+
+print(Go["2"])
+print(Go["def"])
+
+print(Python["4"])
+print(Python["def"])
+
+print(Java["1"])
+print(Java["def"])
+
+print(CSS["5"])
+print(CSS["def"])
